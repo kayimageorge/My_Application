bd63904b0e556daf2f73a6693b73510c6e22a16b
kayimageorge <61316726+kayimageorge@users.noreply.github.com>
2020-02-21 16:09:47 +0300

    Added a Log

--- app/src/main/java/eyit/yit/myapplication/MainActivity.java
+++ app/src/main/java/eyit/yit/myapplication/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("Fs","First Commit Bug");
+        Log.e("Gt","Changes from git");
     }
 }
